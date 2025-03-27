67151d43f4ce9c5b43e49d1f4f0a0a5b82925591
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:26:30 +0800

    Ask the user for the inputs

--- alternative_for_center.py
+++ alternative_for_center.py
@@ -1,4 +1,7 @@
 # Ask the user to input a string and the total width of the output
+string = input("Please enter your string here: ")
+width = int(input("Please enter your desired width here: "))
+
 # Check if width is shorter than the string then print the string without changes
 # If width is longer then ensure that width is equal to the characters + padding on the left + padding on the right
 # Print the output when centered
