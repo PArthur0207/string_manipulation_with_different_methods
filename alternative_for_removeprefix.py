0363680a4c03772e79f66b2b92addf459d2b7845
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 14:48:47 +0800

    Fixed the input error from last commit. And checks if the prefix is in the string

--- alternative_for_removeprefix.py
+++ alternative_for_removeprefix.py
@@ -1,6 +1,10 @@
 # Ask the user to input a string and the prefix to remove
-string = ("Please input your string here: ")
-prefix = ("Please input the prfix to be removed from the string: ")
+string = input("Please input your string here: ")
+prefix = input("Please input the prfix to be removed from the string: ")
 
 # Check if the start of the string matches with the prefix
+if string[:len(prefix)] == prefix:
+    print("The prefix is in the string")
+else:
+    print("The prefix is not in the string")
 # Print the string with the prefix removed if it matches, print the string without change if it does not
