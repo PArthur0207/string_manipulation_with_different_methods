ef57c3115ed5cbe075d7505eb15a34a26ce68f76
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 18:10:38 +0800

    Capitalize the 0th character and turn the rest into lowercase and print the result

--- alternative_for_capitalize.py
+++ alternative_for_capitalize.py
@@ -2,4 +2,7 @@
 string = input("Please input the string here: ")
 
 # Capitalize the 0th character and turn the rest into lowercase
+output = string[0].upper() + string[1:].lower()
+
 # Print the result
+print(output)
