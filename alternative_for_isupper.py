d7e29db637d98fcebdb637465f796dde912719c9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 16:17:20 +0800

    Ask the user for an input

--- alternative_for_isupper.py
+++ alternative_for_isupper.py
@@ -1,3 +1,5 @@
 # Ask the user to input a string to check if all is uppercase
+string = input("Please input a string to check if all is uppercase: ")
+
 # Check for lowercase in every character, false if found, true if not found
 # Print the results
