b4f397b11354472652e1247ee750f01084ca8543
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:11:23 +0800

    Check if the string is longer than the desired width then print the string without changes

--- alternative_for_ljust.py
+++ alternative_for_ljust.py
@@ -6,4 +6,7 @@ width = int(input("Please input your desired width here: "))
 filler = input("Please enter what you want to pad it with: ") or " "
 
 # Check if the width is shorter than the string then make no changes to the string
+if len(string) > width:
+    print(string)
+
 # If the width is longer then pad the spaces on the right using the inputted fill (Reads only the 0th index)
