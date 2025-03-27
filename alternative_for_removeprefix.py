4b8abb9211fb3bc5d2a6a6602da995deed503dc3
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 14:44:12 +0800

    Ask the user to input a string and the prefix to be removed

--- alternative_for_removeprefix.py
+++ alternative_for_removeprefix.py
@@ -1,3 +1,6 @@
 # Ask the user to input a string and the prefix to remove
+string = ("Please input your string here: ")
+prefix = ("Please input the prfix to be removed from the string: ")
+
 # Check if the start of the string matches with the prefix
 # Print the string with the prefix removed if it matches, print the string without change if it does not
