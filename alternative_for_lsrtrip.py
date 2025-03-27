aef4fd180f98331924179e3c54ce42d3f622b8ab
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 14:23:35 +0800

    Asks the user for an input

--- alternative_for_lsrtrip.py
+++ alternative_for_lsrtrip.py
@@ -1,3 +1,4 @@
 # Ask the user to input a string with leading spaces
+string = input("Please input a string with leading spaces: ")
 # Count the leading spaces
 # Print the result starting from the first characer that is not a space
