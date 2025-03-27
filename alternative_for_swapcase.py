96ff33cdc71d60e024981c1957f9561b3302de2a
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:43:56 +0800

    Pseudocode for alternative_for_swapcase

---
+++ alternative_for_swapcase.py
@@ -0,0 +1,5 @@
+# Ask the user for an input of combined lower case and upper case letters
+# For every character in the string check if capital letter and then change it to lowercase using their respective ASCII values
+# Do the same for every lowercase letters turn into uppercase
+# Non alphabetical characters should remain unchanged
+# Print the output
