4cd0b863f46feaeb6495fa544573c949b2904d86
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 18:34:08 +0800

    Capitalize the first letter of each word and print the result

--- alternative_for_title.py
+++ alternative_for_title.py
@@ -4,5 +4,11 @@ string = input("Input your string here: ")
 # Split the string into words
 words = string.split()
 
+output = "" # Initialized output as spaceholder
+
 # Capitalize the first letter of each word
+for i in words:
+    output += i.capitalize() + " "
+
 # Print the result of each word
+print(output)
