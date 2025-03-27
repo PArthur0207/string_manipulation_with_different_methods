230331b889a0dca5d56c83acff29cc8f48609350
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 16:37:34 +0800

    Check if the end of the string matches the suffix

--- alternative_for_endswith.py
+++ alternative_for_endswith.py
@@ -3,4 +3,10 @@ string = input("Please input the string: ")
 suffix = input("Please input the suffix to be checked: ")
 
 # Check if the end of the string matches the suffix
+if string[-len(suffix):] == suffix:
+    ends_with = True
+else:
+    ends_with = False
+
 # Print True or False
+print(ends_with)
