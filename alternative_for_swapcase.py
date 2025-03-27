5bf6b82ff1f0e8a76f195ea76ebcfde965eaf189
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:45:07 +0800

    Asks the user to input a string

--- alternative_for_swapcase.py
+++ alternative_for_swapcase.py
@@ -1,4 +1,6 @@
 # Ask the user for an input of combined lower case and upper case letters
+string = input("Please input your string with upper and lower case letters here: ")
+
 # For every character in the string check if capital letter and then change it to lowercase using their respective ASCII values
 # Do the same for every lowercase letters turn into uppercase
 # Non alphabetical characters should remain unchanged
