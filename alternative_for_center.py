97596cb04a566f27b92b9bf2eb3cc23a3427212b
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:28:42 +0800

    Check if width is shorter than the string then print the string without changes

--- alternative_for_center.py
+++ alternative_for_center.py
@@ -3,5 +3,8 @@ string = input("Please enter your string here: ")
 width = int(input("Please enter your desired width here: "))
 
 # Check if width is shorter than the string then print the string without changes
+if len(string) >= width:
+    print(string)
+    
 # If width is longer then ensure that width is equal to the characters + padding on the left + padding on the right
 # Print the output when centered
