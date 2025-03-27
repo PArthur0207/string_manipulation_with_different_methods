82502dee64249c5f8c07ffd09b0e5bbb942241f4
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 18:26:38 +0800

    Ask the user to input a string

--- alternative_for_title.py
+++ alternative_for_title.py
@@ -1,4 +1,6 @@
 # Ask the user to input a string
+string = input("Input your string here: ")
+
 # Split the string into words
 # Capitalize the first letter of each word
 # Print the result of each word
