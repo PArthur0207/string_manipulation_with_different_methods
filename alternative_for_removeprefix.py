876f22ef37b61e57ab46582be55eff8da1fc42ce
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 14:42:46 +0800

    Pseudocode for alternative_for_removeprefix

---
+++ alternative_for_removeprefix.py
@@ -0,0 +1,3 @@
+# Ask the user to input a string and the prefix to remove
+# Check if the start of the string matches with the prefix
+# Print the string with the prefix removed if it matches, print the string without change if it does not
