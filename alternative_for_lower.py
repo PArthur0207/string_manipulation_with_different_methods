51f250b4b26ea141460101cf85e40b02bbf0c4e3
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 16:07:39 +0800

    Print the string with all lowercase

--- alternative_for_lower.py
+++ alternative_for_lower.py
@@ -1,9 +1,14 @@
 # Ask the user to input a string with capital letters
 string = input("Please enter a string with capital letters: ")
+
+output = "" # Initialized placeholder for the printed output
+
 # Check every character in the string if it is a capital letter
 for character in string:
     if 'A' <= character <= 'Z':
-        character = chr(ord(character) + 32) # Convert to lowercase using the ASCII
-        print(character)
+        output += chr(ord(character) + 32) # Convert to lowercase using the ASCII
+    else:
+        output += character
 
 # Print the result with all lowercase
+print(output)
