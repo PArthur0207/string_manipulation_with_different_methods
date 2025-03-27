be279176fcc613eb0c945e1a5f3bf33708bafe66
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:31:08 +0800

    If width is longer then ensure that width is equal to the characters + padding on the left + padding on the right

--- alternative_for_center.py
+++ alternative_for_center.py
@@ -5,6 +5,11 @@ width = int(input("Please enter your desired width here: "))
 # Check if width is shorter than the string then print the string without changes
 if len(string) >= width:
     print(string)
-    
+
 # If width is longer then ensure that width is equal to the characters + padding on the left + padding on the right
+else:
+    total_padding = width - len(string)
+    left_padding = total_padding // 2
+    right_padding = total_padding - left_padding
+
 # Print the output when centered
