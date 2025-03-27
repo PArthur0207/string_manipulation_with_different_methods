1417cf0771f29650b4d1b896d3fd8eff27822bad
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 16:34:34 +0800

    Ask the user to input a string and the suffix to be checked

--- alternative_for_endswith.py
+++ alternative_for_endswith.py
@@ -1,3 +1,6 @@
 # Ask the user to input a string and the suffix to be checked
+string = input("Please input the string: ")
+suffix = input("Please input the suffix to be checked: ")
+
 # Check if the end of the string matches the suffix
 # Print True or False
