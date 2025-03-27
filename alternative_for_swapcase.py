6305c6d33dd5935ae5b54f209d5c8dd7f74dc8bc
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:56:44 +0800

    Print the output

--- alternative_for_swapcase.py
+++ alternative_for_swapcase.py
@@ -1,15 +1,18 @@
 # Ask the user for an input of combined lower case and upper case letters
 string = input("Please input your string with upper and lower case letters here: ")
 
+output = "" # Initialized placeholder for the output
+
 # For every character in the string check if capital letter and then change it to lowercase using their respective ASCII values
 for character in string:
     if 'A' <= character <= 'Z':
-        print(chr(ord(character) + 32))
+        output += chr(ord(character) + 32)
     # Do the same for every lowercase letters turn into uppercase
     elif 'a' <= character <= 'z':
-        print(chr(ord(character) - 32))
+        output += chr(ord(character) - 32)
     # Non alphabetical characters should remain unchanged
     else:
-        print(character)
+        output += character
 
 # Print the output
+print(output)
