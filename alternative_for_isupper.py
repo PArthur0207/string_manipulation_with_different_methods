cf549a4b9c0a19c1bbd26f8291c8fb0e20520d0e
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 16:21:22 +0800

    Check for lowercase in every character, false if found, true if not found

--- alternative_for_isupper.py
+++ alternative_for_isupper.py
@@ -2,4 +2,11 @@
 string = input("Please input a string to check if all is uppercase: ")
 
 # Check for lowercase in every character, false if found, true if not found
+for character in string:
+    if 'a' <= character <= 'z':
+        all_upper = False
+        break
+    else:
+        all_upper = True
+
 # Print the results
