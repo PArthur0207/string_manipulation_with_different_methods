6e16299ad39150a7cb380b108b364d472b017d44
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:25:08 +0800

    Pseudocode for alternative_for_center

---
+++ alternative_for_center.py
@@ -0,0 +1,4 @@
+# Ask the user to input a string and the total width of the output
+# Check if width is shorter than the string then print the string without changes
+# If width is longer then ensure that width is equal to the characters + padding on the left + padding on the right
+# Print the output when centered
