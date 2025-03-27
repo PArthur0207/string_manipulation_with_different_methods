2795984b6279d9c97101d06a9b6d229c6e1a4469
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 15:56:15 +0800

    Pseudodcode for alternative_for_lower

---
+++ alternative_for_lower.py
@@ -0,0 +1,4 @@
+# Ask the user to input a string with capital letters
+# Check every character in the string if it is a capital letter
+# Convert to lowercase using the ASCII
+# Print the result with all lowercase
