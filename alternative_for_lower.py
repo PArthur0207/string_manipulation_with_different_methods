0c58b72477d37bc88d3419611f4650a4c31bacf6
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 16:00:31 +0800

    Check each character in the string if it's a capital letter

--- alternative_for_lower.py
+++ alternative_for_lower.py
@@ -1,5 +1,9 @@
 # Ask the user to input a string with capital letters
 string = input("Please enter a string with capital letters: ")
 # Check every character in the string if it is a capital letter
+for character in string:
+    if 'A' <= character <= 'Z':
+        print(character)
+
 # Convert to lowercase using the ASCII
 # Print the result with all lowercase
