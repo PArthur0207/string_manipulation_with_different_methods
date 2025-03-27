73229b0e1bf37a814173046687467e9010ce2784
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 15:57:21 +0800

    Ask the user to input a string with capital letters

--- alternative_for_lower.py
+++ alternative_for_lower.py
@@ -1,4 +1,5 @@
 # Ask the user to input a string with capital letters
+string = input("Please enter a string with capital letters: ")
 # Check every character in the string if it is a capital letter
 # Convert to lowercase using the ASCII
 # Print the result with all lowercase
