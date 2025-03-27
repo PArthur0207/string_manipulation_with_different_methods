5d6faa02cc334f22fdd81a3780003883319b31e0
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 14:31:54 +0800

    Print the result starting from the character that is not a space

--- alternative_for_lsrtrip.py
+++ alternative_for_lsrtrip.py
@@ -1,9 +1,10 @@
 # Ask the user to input a string with leading spaces
 string = input("Please input a string with leading spaces: ")
+
 # Count the leading spaces
 space_count = 0
 while string[space_count] == " ":
     space_count += 1
 
-print(space_count)
 # Print the result starting from the first characer that is not a space
+print(string[space_count:])
