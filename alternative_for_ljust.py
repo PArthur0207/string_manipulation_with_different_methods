b19c4303b0a9a5aeb9926a5397c019185bfb6af9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-27 17:02:27 +0800

    Pseudocode for alternative_for_ljust

---
+++ alternative_for_ljust.py
@@ -0,0 +1,4 @@
+# Ask the user to input a string and their desired width for the output
+# Ask the user what to fill the padding with, space if none is entered
+# Check if the width is shorter than the string then make no changes to the string
+# If the width is longer then pad the spaces on the right using the inputted fill (Reads only the 0th index)
