2a523a447b215c785d435842a76b24e62c2fa793
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:09:22 +0800

    Pseudocode for alternative_for_upper

---
+++ alternative_for_upper.py
@@ -0,0 +1,4 @@
+# Ask the user for an input with lowercases
+# Check every character in the string
+# If the character is within a-z convert it into capital letters using their respective ASCII values
+# Print the result in all uppercase
