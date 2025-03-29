493c25f88124a577581747818556064b7e519915
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:37:53 +0800

    Print the resulting string in all uppercase

--- alternative_for_upper.py
+++ alternative_for_upper.py
@@ -1,10 +1,15 @@
 # Ask the user for an input with lowercases
 string = input("Please input a string containing lowercase letters here: ")
 
+output = "" # Initialize placeholder for the output
+
 # Check every character in the string
 for character in string:   
     # If the character is within a-z convert it into capital letters using their respective ASCII values
     if 'a' <= character <= 'z':
-        print(chr(ord(character) - 32))
+        output += (chr(ord(character) - 32))
+    else:
+        output += character
 
 # Print the result in all uppercase
+print(output)
