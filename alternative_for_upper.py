abeb498c9731754ea0affa3fe95aab5a571fea14
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:34:11 +0800

    If the character is within a-z convert it into capital letters using their respective ASCII values

--- alternative_for_upper.py
+++ alternative_for_upper.py
@@ -2,8 +2,9 @@
 string = input("Please input a string containing lowercase letters here: ")
 
 # Check every character in the string
-for character in string:
-    print(character)
-    
-# If the character is within a-z convert it into capital letters using their respective ASCII values
+for character in string:   
+    # If the character is within a-z convert it into capital letters using their respective ASCII values
+    if 'a' <= character <= 'z':
+        print(chr(ord(character) - 32))
+
 # Print the result in all uppercase
