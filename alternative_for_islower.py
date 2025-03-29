93b8d5fcaeaeeee6dfc10e4c540301e3b2144c6e
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:48:05 +0800

    Ask the user for an input

--- alternative_for_islower.py
+++ alternative_for_islower.py
@@ -1,3 +1,5 @@
 # Ask the user to iput a string
+string = input("Please input a string here: ")
+
 # Read every character in the string
 # If a lower case letter is found return false, if not then return true 
