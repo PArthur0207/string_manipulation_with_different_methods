720b7118b4fe23c7a90dfd557cfa91848ad0cf61
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 14:05:23 +0800

    If a lower case letter is found return false, if not then return true

--- alternative_for_islower.py
+++ alternative_for_islower.py
@@ -3,6 +3,11 @@ string = input("Please input a string here: ")
 
 # Read every character in the string
 for character in string:
-    print(character)
-    
+    if 'A' <= character <= 'Z':
+        all_lower = False
+        break
+    else:
+        all_lower = True
+
 # If a lower case letter is found return false, if not then return true 
+print(f"is_lower = {all_lower}")
