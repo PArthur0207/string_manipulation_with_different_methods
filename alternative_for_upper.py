e54df33129e4979c04dc277f46eee71008caab96
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:19:21 +0800

    Check every character in the string

--- alternative_for_upper.py
+++ alternative_for_upper.py
@@ -2,5 +2,8 @@
 string = input("Please input a string containing lowercase letters here: ")
 
 # Check every character in the string
+for character in string:
+    print(character)
+    
 # If the character is within a-z convert it into capital letters using their respective ASCII values
 # Print the result in all uppercase
