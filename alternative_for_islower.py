12e3eeb1226d340b67d7f07f292d4339c209abd5
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:51:30 +0800

    Check every character in the string

--- alternative_for_islower.py
+++ alternative_for_islower.py
@@ -2,4 +2,7 @@
 string = input("Please input a string here: ")
 
 # Read every character in the string
+for character in string:
+    print(character)
+    
 # If a lower case letter is found return false, if not then return true 
