f3d7a671ff233615b4206881c7db1504f1897f7f
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-29 13:13:20 +0800

    Asks the user for an input

--- alternative_for_upper.py
+++ alternative_for_upper.py
@@ -1,4 +1,6 @@
 # Ask the user for an input with lowercases
+string = input("Please input a string containing lowercase letters here: ")
+
 # Check every character in the string
 # If the character is within a-z convert it into capital letters using their respective ASCII values
 # Print the result in all uppercase
