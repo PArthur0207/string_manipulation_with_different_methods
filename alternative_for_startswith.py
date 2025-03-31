4c1e94d44ab5a6a48767e1ded128a035cb9506a4
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 01:18:05 +0800

    Check if the prefixes matches, and print the result

--- alternative_for_startswith.py
+++ alternative_for_startswith.py
@@ -3,4 +3,10 @@ string = input("Please input a string here: ")
 prefix = input("Please input the prefix to be checked: ")
 
 # Check if the end of the string matches the prefix
+if string[:len(prefix)] == prefix:
+    starts_with = True
+else:
+    starts_with = False
+
 # Print True or False
+print(f"starts_with = {starts_with}")
