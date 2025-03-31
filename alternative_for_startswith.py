728e2ff8b1fc96406373d19c3585ceab2b68f5ae
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 01:14:45 +0800

    Ask the user for inputs

--- alternative_for_startswith.py
+++ alternative_for_startswith.py
@@ -1,3 +1,6 @@
 # Ask the user to input a string and the prefix to be checked
+string = input("Please input a string here: ")
+prefix = input("Please input the prefix to be checked: ")
+
 # Check if the end of the string matches the prefix
 # Print True or False
