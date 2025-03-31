1732f8e929b969effa257644ffa5361c2bfc469b
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 01:26:22 +0800

    Pseudocode for alternative_for_zfill

---
+++ alternative_for_zfill.py
@@ -0,0 +1,3 @@
+# Ask the user to input the string and the width to be filled
+# Print the 0s in the beggining of the string if the width is greater than the length of the string
+# No changes in the string if the width is less than the length of the string
