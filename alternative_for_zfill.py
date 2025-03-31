3553b7507e51e59fa785bf1b793e7d0abb436961
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 01:27:49 +0800

    Ask the user for the string and width inputs

--- alternative_for_zfill.py
+++ alternative_for_zfill.py
@@ -1,3 +1,6 @@
 # Ask the user to input the string and the width to be filled
+string = input("Please enter your string here: ")
+width = int(input("Please enter the total width of the output here: "))
+
 # Print the 0s in the beggining of the string if the width is greater than the length of the string
 # No changes in the string if the width is less than the length of the string
