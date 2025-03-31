250ee412731f0a097d5082f684b199d416b7bded
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 01:30:02 +0800

    Print the 0s in the beggining of the string if the width is greater than the length of the string, no changes on the string if not

--- alternative_for_zfill.py
+++ alternative_for_zfill.py
@@ -3,4 +3,9 @@ string = input("Please enter your string here: ")
 width = int(input("Please enter the total width of the output here: "))
 
 # Print the 0s in the beggining of the string if the width is greater than the length of the string
+if width > len(string):
+    print("0" * (width - len(string)) + string)
+
 # No changes in the string if the width is less than the length of the string
+else:
+    print(string)
