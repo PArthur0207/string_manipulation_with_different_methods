f1a81e295060cef64c23bc8ff7bf67d88e4460c7
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 16:39:18 +0800

    Ask the user to input the string and the parameter

--- alternative_for_index.py
+++ alternative_for_index.py
@@ -1,3 +1,6 @@
 # Ask the user to input the string and the parameter to be found
+string = input("Please input your string here: ")
+find = input("Please input the parameter to be found here: ")
+
 # Check all the characters in the sring from the left until the paramter is matched
 # Print the location of the parameter
