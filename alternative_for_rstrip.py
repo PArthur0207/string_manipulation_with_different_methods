b0ba097dc9dca9961408983c2027db84b04b6fa7
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 15:52:38 +0800

    Remove the spaces in the end of the string

--- alternative_for_rstrip.py
+++ alternative_for_rstrip.py
@@ -2,4 +2,7 @@
 string = input("Please input a string with spaces in the end: ")
 
 # Remove the spaces at the end of the string
+while string and string[-1] == " ":
+    string = string[:-1]
+    
 # Print the result  
