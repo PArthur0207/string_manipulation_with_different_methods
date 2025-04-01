0d6d7a15047bbf55e3e3208b794def38b376e666
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 17:01:01 +0800

    Ask the user for an input

--- alternative_for_rindex.py
+++ alternative_for_rindex.py
@@ -1,3 +1,6 @@
 # Asks the user to input the string and the parameter to find
+string = input("Please enter a string here: ")
+find = input("Please input the parameter to be found here: ")
+
 # Look for the parameter within the string starting from the end
 # Print the location of the parameter starting from the end
