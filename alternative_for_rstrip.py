dc22e17930eeb8db2bc1120fa6ff34519e452dab
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 15:50:38 +0800

    Ask the user for a string that has spaces in the end

--- alternative_for_rstrip.py
+++ alternative_for_rstrip.py
@@ -1,3 +1,5 @@
-# ASk the user to input a string
+# Ask the user to input a string
+string = input("Please input a string with spaces in the end: ")
+
 # Remove the spaces at the end of the string
-# Print the result
+# Print the result  
