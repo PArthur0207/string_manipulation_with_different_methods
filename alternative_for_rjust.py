4152ad5dcc49ae71f37c33587503e5bf9ea3e5b9
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 16:28:21 +0800

    Check if the length of the string is less than the width then add the spaces, no change if not and prints the result

--- alternative_for_rjust.py
+++ alternative_for_rjust.py
@@ -1,6 +1,10 @@
 # Ask the user to input the string and the width
 string = input("Please input ypur string here: ")
-width = input("Please input the total width of the output here: ")
+width = int(input("Please input the total width of the output here: "))
 
 # Check if the length of the string is less than the width then add the spaces, no change if not
+if len(string) < width:
+    result = " " * (width - len(string)) + string
+
 # Print the results
+print(result)
