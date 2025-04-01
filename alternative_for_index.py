9f46ff4b98dd31bdac2862021b1ad647994b7ec4
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 16:35:23 +0800

    Pseudocode for alternative_for_index

---
+++ alternative_for_index.py
@@ -0,0 +1,3 @@
+# Ask the user to input the string and the parameter to be found
+# Check all the characters in the sring from the left until the paramter is matched
+# Print the location of the parameter
