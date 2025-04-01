ad77f31bb40649d25b59d40c48c211bb7d9d704f
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 15:03:55 +0800

    Pseudocode for alternative_for_count

---
+++ alternative_for_count.py
@@ -0,0 +1,4 @@
+# Ask the user to input a string and the parameter to find
+# Loop accross the string to find the parameter
+# Count the number of times it is found in the string
+# Print the results
