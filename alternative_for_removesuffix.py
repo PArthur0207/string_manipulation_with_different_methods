f202aa5308a7282d0949c5457141157a6252faa8
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 16:09:50 +0800

    Ask the user for the inputs

--- alternative_for_removesuffix.py
+++ alternative_for_removesuffix.py
@@ -1,3 +1,6 @@
 # Ask the user to input a string and the suffix to be removed
+string = input("Please input your string here: ")
+suffix = input("Please input the suffix to be removed here: ")
+
 # Check if the string ends with the suffix, and then remove the suffix from the string
 # Print the result
