9e143381a16d70b471c4266c1d1aea4269b69f6c
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 17:11:53 +0800

    Look for the parameter within the string starting from the end and rint the location of the parameter starting from the end

--- alternative_for_rindex.py
+++ alternative_for_rindex.py
@@ -3,4 +3,11 @@ string = input("Please enter a string here: ")
 find = input("Please input the parameter to be found here: ")
 
 # Look for the parameter within the string starting from the end
+index = -1
+for i in range(len(string) - len(find), -1, -1):
+    if string[i:i + len(find)] == find:
+        index = i
+        break
+
 # Print the location of the parameter starting from the end
+print(f"The parameter: {find}, is in the {index} position")
