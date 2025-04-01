2fbcf13e9c4089f5094b13186b2546a1e0d2f2f0
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 16:47:27 +0800

    Check all the characters in the sring from the left until the paramter is matched and print the location of the parameter

--- alternative_for_index.py
+++ alternative_for_index.py
@@ -3,4 +3,11 @@ string = input("Please input your string here: ")
 find = input("Please input the parameter to be found here: ")
 
 # Check all the characters in the sring from the left until the paramter is matched
+index = -1
+for i in range(len(string) - len(find) + 1):
+    if string[i:i + len(find)] == find:
+        index = i
+        break
+
 # Print the location of the parameter
+print(f"Parameter {find} is in the {index} position")
