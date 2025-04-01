4c903476def1f988cc5c0bfad49051bfea2f954f
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 16:59:12 +0800

    Pseudocode for alternative_for_rindex

---
+++ alternative_for_rindex.py
@@ -0,0 +1,3 @@
+# Asks the user to input the string and the parameter to find
+# Look for the parameter within the string starting from the end
+# Print the location of the parameter starting from the end
