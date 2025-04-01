353a2ad060f1a0b26741b483660bc27eb8f7f7ae
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-04-01 15:05:10 +0800

    Ask the user for the inputs

--- alternative_for_count.py
+++ alternative_for_count.py
@@ -1,4 +1,7 @@
 # Ask the user to input a string and the parameter to find
+string = input("Please input your string here: ")
+parameter = input("Please input the parameters to be found in the string here: ")
+
 # Loop accross the string to find the parameter
 # Count the number of times it is found in the string
 # Print the results
